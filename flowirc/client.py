e8bf9bb083bf9e8ad9ba5f730b4b9b087945ac9e
olle <geek@nerd.sh>
2013-12-03 15:00:45 +0100

    Refactor client, use a plain subclass, use default listeners.
    --HG--
    extra : amend_source : 65677c1206a16f651c96d9807b3baee590c83e99
    extra : histedit_source : 848c74c09522f0f443950d80cf108bab4c12e603

--- flowirc/client.py
+++ flowirc/client.py
@@ -1,4 +1,3 @@
-import signal
 import functools
 from collections import defaultdict
 
@@ -25,64 +24,53 @@ class IRCClientProtocol(asyncio.Protocol):
         self._transport = transport
         self.after_connection_made()
 
-    def _encode(self, data):
-        return str(data).encode()
-
-    def send(self, line):
-        data = self._encode(line)
-        print(data)
-        self._transport.write(data)
+    def send(self, *lines):
+        for line in lines:
+            data = line.encode()
+            self._transport.write(data)
 
     def data_received(self, data):
         lines = data.decode().split("\r\n")
         for line in lines:
-            print(line)
+
             if len(line) <= 2:
-                # This is a blank line, at best.
                 continue
+
             msg = MessageBase.from_str(line)
+
             if msg is not None:
                 asyncio.Task(self.message_received(msg))
 
 
-_listeners = defaultdict(list)
-
-
-def listen_to(message):
-    if callable(message):
-        message = message()
-
-    def decorator(callback):
-        _listeners[message.type()].append((message, asyncio.coroutine(
-            callback)))
-
-        @functools.wraps(callback)
-        def f(*args):
-            return callback(*args)
-
-        return f
-
-    return decorator
-
-
 class IRCClient(IRCClientProtocol):
-    @classmethod
-    def connect(cls, host='localhost', port=6667, ircname="flowirc",
-                ircnick="flowirc", ssl=None, loop=None):
+    def __init__(self, name=None, host='localhost', port=6667,
+                 user="flowirc", nick="flowirc", ssl=None, loop=None):
 
         if loop is None:
             loop = asyncio.get_event_loop()
         if ssl is None:
             ssl = True if port == 6697 else False
 
-        self = cls()
-        self.ircname = ircname
-        self.ircnick = ircnick
-        asyncio.Task(loop.create_connection(
+        self.__listeners = defaultdict(list)
+        self._name = name
+        self._nick = nick
+        self._user = user
+        self.host = host
+        self.port = port
+        self.ssl = ssl
+        self.loop = loop
+
+    def run(self, use_default_listeners=True):
+        if use_default_listeners:
+            mod = importlib.import_module('flowirc.listeners.default')
+            mod.register(self)
+            del mod
+
+        asyncio.Task(self.loop.create_connection(
             lambda: self,
-            host=host,
-            port=port,
-            ssl=ssl))
+            host=self.host,
+            port=self.port,
+            ssl=self.ssl))
 
     def after_connection_made(self):
         self.identify()
@@ -92,19 +80,38 @@ class IRCClient(IRCClientProtocol):
         self.nick()
         self.user()
 
-    def nick(self):
-        self.send(NickMessage(self.ircnick))
+    def nick(self, nick=None):
+        if nick is not None and nick != self._nick:
+            self._nick = nick
+        self.send(NickMessage(self._nick))
+
+    def user(self, user=None):
+        if user is not None and user != self._user:
+            self._user = user
+        self.send(UserMessage(self._nick, self._user))
 
     def join(self, channel):
         self.send(JoinMessage(channel))
 
-    def user(self):
-        self.send(UserMessage(self.ircnick, self.ircname))
+    def listen_to(self, message):
+        if callable(message):
+            message = message()
+
+        def decorator(callback):
+            self.__listeners[message.type()]. \
+                append((message, asyncio.coroutine(callback)))
+
+            @functools.wraps(callback)
+            def f(*args):
+                return callback(*args)
+
+            return f
+
+        return decorator
 
     @asyncio.coroutine
     def message_received(self, msg):
-        listeners = _listeners[msg.type()]
-        print("Listeners for messagetype {}: {}".format(msg.type(), listeners))
+        listeners = self.__listeners[msg.type()]
 
         coroutines = [callback(msg) for message, callback in
                       listeners if self.matches(message, msg)]
@@ -115,16 +122,3 @@ class IRCClient(IRCClientProtocol):
 
     def matches(self, message1, message2):
         return True
-
-
-@listen_to(PingMessage)
-def song(msg):
-    reply = PongMessage(msg)
-    return reply
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.add_signal_handler(signal.SIGINT, loop.stop)
-    IRCClient.connect()
-    loop.run_forever()
